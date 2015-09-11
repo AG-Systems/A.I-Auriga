7a625b68af226bfc43da97058db4e154ab9be128
Unknown <AG-Systems@users.noreply.github.com>
2015-09-11 13:18:19 -0700

    Update Version: 0.4.py

--- Pre-Alpha/Version: 0.4.py
+++ Pre-Alpha/Version: 0.4.py
@@ -1,1 +1,22 @@
+import random
+var = raw_input("Enter something: ")
+#Take input from the user
+splitted = var.split()
+#splits the string
+first = splitted[0]
+second = splitted[1]
+#Takes the first 2 words in a string
+alg = [first, second]
+#Insert into a list
+rand_alg = alg[random.randrange(len(alg))]
+#Pick one randomly
+
+
+store_var = []
+for i in range(5):
+    store_var.append( input("String will be inputed ") )
+for line in store_var:
+    print(line
+    #PRINT everything and let the program try to guess what you are going to type next as it learns from you.
+
 
